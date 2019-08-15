460fd5645078fcbceeee8560a362c830aef2edb3
Chris Guthrie <guthricb@gmail.com>
2019-08-15 01:41:23 +0000

    Lesson 19 - Style the App

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,7 +6,7 @@ class GramsController < ApplicationController
 	end
 
 	def index
-		
+		@grams = Gram.all
 	end
 
 	def destroy
@@ -53,11 +53,7 @@ class GramsController < ApplicationController
 	private
 
 	def gram_params
-		params.require(:gram).permit(:message)		
-	end
-
-	def render_not_found(status=:not_found)
-		render plain: "{status.to_s.titleize} :(", status: status
+		params.require(:gram).permit(:message, :picture)		
 	end
 
 end
